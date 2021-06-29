4dd3e38d7fe7a573c17fbf4deba8a413477f51c1
fawall <fabiolopes7844@gmail.com>
2021-06-29 18:06:12 -0300

    Adicionado DI

--- src/Heart.API/Startup.cs
+++ src/Heart.API/Startup.cs
@@ -11,6 +11,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using AutoMapper;
+using Heart.Services.DTO;
+using Heart.Domain.Entities;
+using Heart.API.ViewModels;
+using Heart.Services.Interfaces;
+using Heart.Services.Services;
+using Heart.Infra.Repositories;
+using Heart.Infra.Interfaces;
 
 namespace Heart.API
 {
@@ -32,6 +40,23 @@ namespace Heart.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Heart.API", Version = "v1" });
             });
+
+            #region AutoMapper
+
+            var autoMapperConfig = new MapperConfiguration(cfg =>{
+                cfg.CreateMap<User, UserDTO>().ReverseMap();
+                cfg.CreateMap<CreateUserViewModel, UserDTO>().ReverseMap();
+            }).CreateMapper();
+
+            services.AddSingleton(autoMapperConfig);
+            #endregion 
+
+            #region DI
+            services.AddScoped<IUserService, UserServices>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            #endregion
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
