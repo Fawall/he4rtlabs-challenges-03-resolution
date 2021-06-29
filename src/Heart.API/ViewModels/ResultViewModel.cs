d70cfe66803892f91dd6b62e4c3f21c04b6e63b1
fawall <fabiolopes7844@gmail.com>
2021-06-29 16:13:42 -0300

    Criado ResultViewModel

---
+++ src/Heart.API/ViewModels/ResultViewModel.cs
@@ -0,0 +1,9 @@
+namespace Heart.API.ViewModels
+{
+    public class ResultViewModel
+    {
+        public string Message {get; set;}
+        public bool Sucess { get; set; }
+        public dynamic Data { get; set; }
+    }
+}
