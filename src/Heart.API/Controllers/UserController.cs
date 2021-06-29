fe19509a209960297f826db87b0577a0b4e1cd5d
fawall <fabiolopes7844@gmail.com>
2021-06-29 16:45:02 -0300

    Criado UserController e iniciado método Create

---
+++ src/Heart.API/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Heart.Services.DTO;
+using Heart.API.ViewModels;
+using Heart.Services.Interfaces;
+using AutoMapper;
+using System.Threading.Tasks;
+using Heart.Core.Exceptions;
+using Heart.API.Utilities;
+using System;
+
+namespace Heart.API.Controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public UserController(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("/api/v1/users/create")]
+        public async Task<IActionResult> Create([FromBody]CreateUserViewModel userViewModel)
+        {
+            try
+            {
+                var userDTO = _mapper.Map<UserDTO>(userViewModel);
+                var userCreated = await _userService.Create(userDTO);
+
+                return Ok(new ResultViewModel
+                {
+                    Message = "Usuário criado com sucesso",
+                    Sucess = true,
+                    Data = userCreated
+                });
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(Responses.DomainErrorMessage(ex.Message, ex.Errors));
+            }
+            catch(Exception)
+            {
+                return StatusCode(500, Responses.ApplicationErrorMessage());
+            }
+        }
+
+
+    }
+}
