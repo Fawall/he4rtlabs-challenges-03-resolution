f354dc4d7fabfe3e7d16e16ca6025a14b719c948
fawall <fabiolopes7844@gmail.com>
2021-06-30 17:08:52 -0300

    criado método para verificar se o usuário existe

--- src/Heart.Infra/Repositories/UserRepository.cs
+++ src/Heart.Infra/Repositories/UserRepository.cs
@@ -5,15 +5,37 @@ using Heart.Infra.Interfaces;
 using System.Data.SqlClient;
 using System.Configuration;
 using System;
+using Heart.Infra.Databases;
 
 namespace Heart.Infra.Repositories
 {
-    public class UserRepository : BaseRepository<User>
+    public class UserRepository : BaseRepository<User>, IUserRepository
     {
-        string conn = ConfigurationManager.ConnectionStrings["SqlConnection"].ConnectionString;
         public async Task<User> GetByEmail(string email)
         {
-            throw new System.NotImplementedException();          
+            User user = null;
+            string queryString = @"SELECT email FROM [Usuarios] WHERE email=@email";
+            
+            using(SqlConnection sqlConnection = new SqlConnection(Conexao()))
+            {
+                await sqlConnection.OpenAsync();
+                using(SqlCommand cmd = new SqlCommand(queryString, sqlConnection))
+                {
+                    cmd.Parameters.AddWithValue("@email", email);
+                    SqlDataReader dr = await cmd.ExecuteReaderAsync();
+
+                    if (dr.HasRows)
+                    {
+                        user = new User(email);
+                        return user;
+                    }
+
+                    await sqlConnection.CloseAsync();
+                    await dr.CloseAsync();
+
+                    return user;
+                }
+            }
         }           
 
         public Task<List<User>> SearchByEmail(string email)
