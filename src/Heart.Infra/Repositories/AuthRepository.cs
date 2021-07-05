d610b6d3de3d49359c6d5faa0c88156135577cd4
fawall <fabiolopes7844@gmail.com>
2021-07-05 14:11:50 -0300

    adicionado verificação de senha com hash

--- src/Heart.Infra/Repositories/AuthRepository.cs
+++ src/Heart.Infra/Repositories/AuthRepository.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using Heart.Infra.Database;
 using System.Data.SqlClient;
 using BCrypt.Net;
+using System.Data;
 
 namespace Heart.Infra.Repositories
 {
@@ -11,7 +12,8 @@ namespace Heart.Infra.Repositories
     {
         public async Task<bool> Login(string email, string password)
         {
-            string queryString = @"SELECT Email,Password FROM [Usuarios] WHERE Email=@email AND Password=@password";
+            // string queryString = @"SELECT Email,Password FROM [Usuarios] WHERE Email=@email AND Password=@password";
+            string queryString = @"SELECT Password FROM [Usuarios] WHERE Email=@email";
 
             using(SqlConnection sqlConnection = new SqlConnection(Conexao()))
             {
@@ -19,16 +21,17 @@ namespace Heart.Infra.Repositories
                 using(SqlCommand cmd = new SqlCommand(queryString, sqlConnection))
                 {
                     cmd.Parameters.AddWithValue("@email", email);
-                    cmd.Parameters.AddWithValue("@password", password);
-                    
-                    SqlDataReader dr = await cmd.ExecuteReaderAsync();
 
-                    if(dr.HasRows)
-                        return true;
-                    
-                    await sqlConnection.CloseAsync();
-                    await dr.CloseAsync();
-                    
+                    var hashedPassword = (string) await cmd.ExecuteScalarAsync();
+
+                    if(hashedPassword != null) 
+                    {
+                        bool verify = BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+                        if (verify != false)
+                            return true;
+                    }
+                                     
+                    await sqlConnection.CloseAsync();                    
                     return false;
                 }
             }
